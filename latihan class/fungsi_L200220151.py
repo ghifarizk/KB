e5cc47d03d7bab3e7ed3c1753d7ca4bbfe71424a
rohmad151 <rohmadroziqun0410@gmail.com>
2023-01-02 21:06:40 +0700

    rohmad

---
+++ latihan class/fungsi_L200220151.py
@@ -0,0 +1,8 @@
+def hitung_luas_segitiga():
+    print('komputer akan membantu anda menghitung luas tabung')
+    alas = float(input('masukkan alas'))
+    tinggi = float(input('masukkan tinggi'))
+    luas = alas*tinggi / 2
+    print('luas segitiga', luas, '\n')
+
+hitung_luas_segitiga()
