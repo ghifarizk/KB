e9e5f43b8f9c1db7487372b5d75099b6cd973de9
RaffieMA <121432096+RaffieMA@users.noreply.github.com>
2023-01-03 07:19:23 +0700

    Add files via upload

---
+++ latihan class/Fungsi.L200220136.py
@@ -0,0 +1,8 @@
+def hitung_Keliling_persegipanjang():
+    print('komputer akan membantu anda menghitung Keliling Persegi Panjang')
+    panjang = float(input('masukkan panjang'))
+    lebar= float(input('masukkan lebar'))
+    keliling = 2*panjang + 2*lebar
+    print('Keliling Persegi Panjang', keliling, '\n')
+
+hitung_Keliling_persegipanjang()
